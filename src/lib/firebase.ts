56acecd60086981737c4ba4d3bdebd85361ba3bf
Arvid Rapp <arvidrapp@MacBook-Air-2.local>
2025-11-15 13:58:53 +0100

    Use env variable for Firebase API key

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCBvAD-MICTfMtanfFcjYAGmYtTf1xIBQw",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "verhandlung-2.firebaseapp.com",
   projectId: "verhandlung-2",
   storageBucket: "verhandlung-2.firebasestorage.app",
-  messagingSenderId: "226056568545",
-  appId: "1:226056568545:web:48c2a51962dd3d39508edc",
-  measurementId: "G-5F5CM1P9ML", // optional, kann auch weg
+  messagingSenderId: "226065560845",
+  appId: "1:226065560845:web:48c2a51962dd3d9508edc",
+  measurementId: "G-5FSCI9M9ML",
 };
 
 const app = initializeApp(firebaseConfig);
